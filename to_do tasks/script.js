1ce214173510f672167ac73b87961c3eb497b471
Rohit Mohadikar <rohitmohadikar897@gmail.com>
2025-06-11 16:39:31 +0530

    Update script.js

---
+++ to_do tasks/script.js
@@ -0,0 +1,34 @@
+let inputBox = document.querySelector('#task-input');
+let add_btn = document.querySelector('button');
+let listContainer = document.querySelector('#task-list');
+add_btn.addEventListener('click', () => {
+    if (inputBox.value === "") {
+        alert('Enter tasks in box')
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+
+        listContainer.appendChild(li);
+        li.appendChild(span);
+        saveData();
+        inputBox.value = '';
+    }
+});
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checket');
+    } else if (e.target.tagName === 'SPAN') (
+        e.target.parentElement.remove()
+    );
+    saveData();
+})
+function saveData() {
+    localStorage.setItem('data', listContainer.innerText)
+};
+function showData() {
+    listContainer.innerText = localStorage.setItem('data')
+};
+showData();
